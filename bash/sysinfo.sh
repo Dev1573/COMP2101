2f1f42f568954934c6c72d2a4f6c709869767af4
Dev <pateldevk06@gmail.com>
2022-06-09 16:31:22 -0400

    Working with data in bash

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,13 +2,12 @@
 # My fourth script - sysinfo.sh
 # This coding will shows different strings
 
-#Let's make variable for hostname
-hn=$(hostname)
-echo "Host Name: $hn"
+#First we will creat Heading
+echo "Report for My Virtual Machine"
 
 #We will use some extantion to get precise information 
 dn=$(hostname --fqdn)
-echo "Domain Name of VM: $dn"
+echo "FQDN: $dn"
 
 #Now it's time to use uname variable to get os name
 os=$(uname -om)
@@ -19,7 +18,5 @@ ip=$(ip addr | awk 'NR=="9" {print$2}')
 echo "Virtual machine IP ADDRESS: $ip"
 
 #Information of filesystem can be gotten from df command
-rt=$(df -h)
-echo "Status of root filesystem: "
-echo "$rt"
-
+rt=$(df -h | awk 'NR=="2" {print $4}')
+echo "Free space in root Filesystem: $rt"
