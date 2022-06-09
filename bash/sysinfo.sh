7df422c60f97d2b41dac38cf11d4333e5ceb4fd6
Dev <pateldevk06@gmail.com>
2022-06-09 16:16:48 -0400

    Bas Begginner Scripting

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,15 +2,24 @@
 # My fourth script - sysinfo.sh
 # This coding will shows different strings
 
-cat <<EOF
-Host Name:
-EOF
-hostname
-echo "Domain Name:Dev-Virtualbox"
-echo "Operating System name and version: "
-lsb_release -d
-echo "LAN IP ADDRESS: "
-ip a s enp0s3 | awk '/inet /{print$2}'
-echo "Root File Status:"
-df -h
+#Let's make variable for hostname
+hn=$(hostname)
+echo "Host Name: $hn"
+
+#We will use some extantion to get precise information 
+dn=$(hostname --fqdn)
+echo "Domain Name of VM: $dn"
+
+#Now it's time to use uname variable to get os name
+os=$(uname -om)
+echo "Information about Operating system: $os"
+
+#For ip address there is a command $ip addr
+ip=$(ip addr | awk 'NR=="9" {print$2}')
+echo "Virtual machine IP ADDRESS: $ip"
+
+#Information of filesystem can be gotten from df command
+rt=$(df -h)
+echo "Status of root filesystem: "
+echo "$rt"
 
